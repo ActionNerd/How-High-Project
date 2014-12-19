cf213064a41ce42a030a46dda0bffbcf49112780
acummiskey <acummiskey@acummiskey-5.local>
2014-12-19 14:20:39 -0500

    New

---
+++ howhighlog.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import datetime
+
+import Adafruit_BMP.BMP085 as BMP085
+
+FREQUENCY_SECONDS     = 30
+
+bmp = BMP085.BMP085()
+
+while True:
+
+    f=open('highlog.txt','a')
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y/%m/%d %H:%M")
+    temp = bmp.read_temperature()
+    pressure = bmp.read_pressure()
+    altitude = bmp.read_altitude()
+    print datetime.datetime.now()
+    print 'Temperature: {0:0.1F} C'.format(temp)
+    print 'Pressure:    {0:0.1F} Pa'.format(pressure)
+    print 'Altitude:    {0:0.1F} m'.format(altitude)
+    f.write (str(temp))
+    f.close()
+
+    time.sleep(FREQUENCY_SECONDS)
